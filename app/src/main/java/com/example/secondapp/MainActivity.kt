514cb69a290f4ea25d51da536725aab68b50f111
aakihabara <Astral5796@gmail.com>
2023-03-03 22:23:59 +1100

    23_lab2

--- app/src/main/java/com/example/secondapp/MainActivity.kt
+++ app/src/main/java/com/example/secondapp/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.secondapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 
@@ -12,27 +13,89 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var true_button: Button
     private lateinit var false_button:Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(Questions(R.string.question_australia, true),
+        Questions(R.string.question_oceans, true),
+        Questions(R.string.question_mideast, false),
+        Questions(R.string.question_africa, false),
+        Questions(R.string.question_asia, true),
+        Questions(R.string.question_america, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun toastMessage(text: String){
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
+
+        fun buildTextView(){
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        }
+
+        fun incIndex(){
+            currentIndex = (currentIndex + 1) % questionBank.size
+            buildTextView()
+        }
+
+        fun decIndex(){
+            if (currentIndex == 0){
+                currentIndex = questionBank.size - 1
+            }
+            else {
+                currentIndex = (currentIndex - 1) % questionBank.size
+            }
+            buildTextView()
+        }
+
         true_button = findViewById(R.id.true_button)
         false_button = findViewById(R.id.false_button)
-
+        prevButton = findViewById(R.id.prev_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         true_button.setOnClickListener{
-            val toast_value = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast_value.setGravity(Gravity.TOP, 0, 0)
-            toast_value.show()
+
+            if(questionBank[currentIndex].answer){
+                toastMessage("Correct")
+            }
+            else {
+                toastMessage("Incorrect")
+            }
+
+            incIndex()
+
         }
 
         false_button.setOnClickListener{
-            val toast_value = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
-            toast_value.setGravity(Gravity.TOP, 0, 0)
-            toast_value.show()
+
+            if(!questionBank[currentIndex].answer){
+                toastMessage("Correct")
+            }
+            else {
+                toastMessage("Incorrect")
+            }
+
+            incIndex()
+
         }
 
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
 
+        nextButton.setOnClickListener{
+            incIndex()
+        }
+
+        prevButton.setOnClickListener{
+            decIndex()
+        }
     }
 }
---
+++ app/src/main/java/com/example/secondapp/Questions.kt
@@ -0,0 +1,7 @@
+package com.example.secondapp
+
+import androidx.annotation.StringRes
+
+data class Questions(@StringRes val textResId: Int, val answer: Boolean){
+
+}
